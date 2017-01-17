527d197b3a3f2676d4418c6d5abf32b376b89f2e
Aydin Gungordu <aydinbg@gmail.com>
2017-01-17 21:05:20 +0000

    Safe initialization

--- src/main/java/com/gt/main/DemographicsCalculator.java
+++ src/main/java/com/gt/main/DemographicsCalculator.java
@@ -15,7 +15,7 @@ import java.util.Objects;
  */
 public class DemographicsCalculator {
 
-    protected List<Person> people = new ArrayList<>();
+    protected final List<Person> people = new ArrayList<>();
 
     public DemographicsCalculator(final String filename) {
         initializePeopleList(filename);
